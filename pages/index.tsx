04c43becc1546329edadbe1158238254a5966b38
MilanBarande <milan.barande@gmail.com>
2021-12-07 16:44:20 +0100

    :lipstick: add stars background

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,30 +5,21 @@ import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Mettaway</title>
         <meta name="description" content="Official Mettaway festival website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className={styles.stars}/>
+      <div className={styles.stars2} />
+      <div className={styles.stars3} />
+  
       <main className={styles.main}>
         <h1 className={styles.glitch} data-text="Recharging...">Recharging...</h1>
       </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
-    </div>
+    </>
   )
 }
 
