03efa8b034ea97d4ac7eef33350276c86d990c07
MilanBarande <milan.barande@gmail.com>
2022-02-11 17:40:09 +0100

    add icon on link

--- components/HomeDates.tsx
+++ components/HomeDates.tsx
@@ -1,4 +1,5 @@
 import styles from '../styles/HomeDates.module.scss';
+import RocketIcon from './RocketIcon';
 import Title from './Title';
 import TravelBack from './TrabelBack';
 
@@ -8,7 +9,7 @@ const HomeDates = () => (
     <h2 className={styles.subtitle}>Voyage #2</h2>
     <span className={styles.dates}>12-16</span>
     <span className={styles.dates}>05|22</span>
-    <a href='https://forms.gle/3bLx5p9tfBtJwAyk8' className={styles.link}>Fly with us</a>
+    <a href='https://forms.gle/3bLx5p9tfBtJwAyk8' className={styles.link}>Fly with us<RocketIcon height={36} width={36} fill='white' style={{marginLeft: '10px'}} /></a>
     <TravelBack className={styles.whiteOnHover} />
   </div>
 )
