0473f3f0680aa477f0901cec6fdb6e73b58163aa
MilanBarande <milan.barande@gmail.com>
2022-05-25 10:55:26 +0200

    set new soundcloud playlist

--- components/TravelBack.tsx
+++ components/TravelBack.tsx
@@ -3,7 +3,7 @@ import SoundcloudIcon from './SoundcloudIcon';
 
 const TravelBack = ({className}: {className?:string}) => (
   <div className={styles.container}>
-    <a href='https://soundcloud.com/delucs-1/sets/mettaway' className={`${styles.link} ${className}`}>
+    <a href='https://soundcloud.com/taylla-yamagucci/sets/mettaway-voayge-2-laser-jungle' className={`${styles.link} ${className}`}>
       <span>Travel back in time</span>
       <SoundcloudIcon height={60} width={60} />
     </a>
