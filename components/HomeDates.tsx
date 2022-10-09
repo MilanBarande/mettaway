0d63838e2e956b9591993b3d5992826a77ca2323
MilanBarande <milan.barande@gmail.com>
2022-10-09 14:51:17 +0200

    testing animation

--- components/HomeDates.tsx
+++ components/HomeDates.tsx
@@ -1,11 +1,34 @@
 import styles from '../styles/HomeDates.module.scss';
 import Title from './Title';
 import TravelBack from './TravelBack';
+import styled from 'styled-components';
+
+const Dot = styled.span<{ i: number }>`
+  position: absolute;
+  top: calc(80px * ${props => props.i});
+  left: calc(80px * ${props => props.i});
+  right: calc(80px * ${props => props.i});
+  bottom: calc(80px * ${props => props.i});
+  
+  &:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: -8px;
+    width: 15px;
+    height: 15px;
+    background: #F00;
+    border-radius: 50%;
+  }
+`
 
 const HomeDates = () => {
   return (
   <>
     <div className={styles.container}>
+      <Dot i={1} className={styles.dot} />
+      <Dot i={2} className={styles.dot} />
+      <Dot i={3} className={styles.dot} />
       <Title className={styles.title} />
       <h2 className={styles.subtitle}>voyage #3</h2>
       <div className={styles.box}>
