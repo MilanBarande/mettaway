4ffb05070cb7a9f226ecc87cd291cc351edabf74
MilanBarande <milan.barande@gmail.com>
2022-10-09 13:31:46 +0200

    new desktop layout

--- components/HomeDates.tsx
+++ components/HomeDates.tsx
@@ -1,23 +1,28 @@
 import styles from '../styles/HomeDates.module.scss';
-import RocketIcon from './RocketIcon';
 import Title from './Title';
 import TravelBack from './TravelBack';
 
-const HomeDates = () => (
-  <div className={styles.container}>
-    <Title className={styles.title} />
-    <h2 className={styles.subtitle}>voyage #3</h2>
-    <span className={styles.dates}>01-05</span>
-    <div className={styles.dates}>
-      <span style={{marginRight:'60px'}}>12</span>
-      2022
+const HomeDates = () => {
+  return (
+  <>
+    <div className={styles.container}>
+      <Title className={styles.title} />
+      <h2 className={styles.subtitle}>voyage #3</h2>
+      <div className={styles.box}>
+        <span className={styles.destination}>destination</span>
+        <span className={styles.darkpalm}>D4rk P4lm</span>
+        <span className={styles.dates}>01-05</span>
+        <div className={styles.dates}>
+          <span style={{marginRight:'30px'}}>12</span>
+          2022
+        </div>
+        <a href='https://tally.so/r/w44MYb' className={styles.link}>
+          fly with us →
+        </a>
+      </div>
+      <TravelBack className={styles.soundcloudLink} />
     </div>
-    <a href='https://forms.gle/3bLx5p9tfBtJwAyk8' className={styles.link}>
-      explore with us
-      <RocketIcon height={42} width={42} fill='#ababab' />
-    </a>
-    <TravelBack className={styles.soundcloudLink} />
-  </div>
-)
+  </>
+)}
 
 export default HomeDates;
