5b16606e2c6e3b748324d7ca7ef1d186bc9c84f9
MilanBarande <milan.barande@gmail.com>
2022-10-13 11:11:12 +0200

    target blank for soundcloud link

--- components/TravelBack.tsx
+++ components/TravelBack.tsx
@@ -3,7 +3,7 @@ import SoundcloudIcon from './SoundcloudIcon';
 
 const TravelBack = ({className}: {className?:string}) => (
   <div className={styles.container}>
-    <a href='https://soundcloud.com/taylla-yamagucci/sets/mettaway-voayge-2-laser-jungle' className={`${styles.link} ${className}`}>
+    <a target='_blank' rel="noreferrer noopener" href='https://soundcloud.com/taylla-yamagucci/sets/mettaway-voayge-2-laser-jungle' className={`${styles.link} ${className}`}>
       <span>travel back in time</span>
       <SoundcloudIcon height={60} width={60} />
     </a>
