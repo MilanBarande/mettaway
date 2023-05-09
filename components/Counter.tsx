a739062175f0dc15a53e73e9094a688b048c2edd
MilanBarande <milan.barande@gmail.com>
2023-05-09 14:02:04 +0200

    only show registered people

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,15 +1,11 @@
 import styles from '../styles/Counter.module.scss';
 
 export default function Counter({registered, paid}: {registered: number, paid: number}) {
-  const pending = registered - paid;
-  const seatsLeft = 152 - registered;
-  
   return (
     <div className={styles.counter}>
       <div className={styles.box}>
-        <p>{paid} seats booked</p>
-        <p>{pending} seats reserved (payment pending)</p>
-        <p>{seatsLeft} seats available</p>
+        <p>{registered} seats booked</p>
+        <p>Last call to get on board!</p>
       </div>
     </div>
   )
