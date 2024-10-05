b7c3e76262d7e616f2494bf7e3d4be3d8123b161
Milan <milan.barande@gmail.com>
2024-10-05 10:53:31 +0200

    :lipstick: blurry background for readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,18 +16,18 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center p-4 sm:p-8 md:p-16 lg:p-24 min-h-screen">
       <h1
-        className={`${monoton.className} text-[150px] leading-none text-center text-[#FFFFFF] mb-6 title-glow`}
+        className={`${monoton.className} text-[150px] leading-none text-center text-[#FFFFFF] mb-6 title-glow text-with-blur`}
       >
         <span className="block">Metta</span>
         <span className="block">Games</span>
       </h1>
       <span
-        className={`${turretRoad.className} text-[60px] text-[#FFFFFF] mt-4 date-glow mb-20`}
+        className={`${turretRoad.className} text-[60px] text-[#FFFFFF] mt-4 date-glow mb-20 text-with-blur`}
       >
         21.-25.11.2024
       </span>
       <span
-        className={`${turretRoad.className} text-black date-glow text-[60px] font-bold`}
+        className={`${turretRoad.className} text-black text-[60px] font-bold date-glow`}
       >
         12 / 140 SPOTS AVAILABLE
       </span>
