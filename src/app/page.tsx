30eb5101fea69b39c7460d8a8fe9adb65fe1bd6a
Milan <milan.barande@gmail.com>
2024-10-05 13:50:16 +0200

    :lipstick: reduce texts sizes on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,22 +15,24 @@ const turretRoad = Turret_Road({
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center p-4 sm:p-8 md:p-16 lg:p-24 min-h-screen">
-      <h1
-        className={`${monoton.className} text-[150px] leading-none text-center text-[#FFFFFF] mb-6 title-glow text-with-blur`}
-      >
-        <span className="block">Metta</span>
-        <span className="block">Games</span>
-      </h1>
-      <span
-        className={`${turretRoad.className} text-[60px] text-[#FFFFFF] mt-4 date-glow mb-20 text-with-blur`}
-      >
-        21.-25.11.2024
-      </span>
-      <span
-        className={`${turretRoad.className} text-black text-[60px] font-bold date-glow`}
-      >
-        12 / 140 SPOTS AVAILABLE
-      </span>
+      <div className="max-w-full text-center text-with-blur">
+        <h1
+          className={`${monoton.className} text-[40px] sm:text-[80px] md:text-[120px] lg:text-[150px] leading-none text-center text-[#FFFFFF] mb-4 sm:mb-6 title-glow`}
+        >
+          <span className="block">Metta</span>
+          <span className="block">Games</span>
+        </h1>
+        <span
+          className={`${turretRoad.className} text-[20px] sm:text-[30px] md:text-[45px] lg:text-[60px] text-[#FFFFFF] mt-2 sm:mt-4 date-glow mb-6 sm:mb-10 md:mb-20 block`}
+        >
+          21.-25.11.2024
+        </span>
+        <span
+          className={`${turretRoad.className} text-black text-[16px] sm:text-[24px] md:text-[40px] lg:text-[60px] font-bold date-glow block`}
+        >
+          12 / 140 SPOTS AVAILABLE
+        </span>
+      </div>
     </main>
   );
 }
