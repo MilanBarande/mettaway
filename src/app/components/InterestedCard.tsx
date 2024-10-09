8205420394023200dbc38e61cef92c3b395be8db
Milan <milan.barande@gmail.com>
2024-10-09 22:28:09 +0200

    hide signup at 140 registered

--- src/app/components/InterestedCard.tsx
+++ src/app/components/InterestedCard.tsx
@@ -56,20 +56,22 @@ const InterestedCard: React.FC = () => {
       >
         {isLoading ? renderWavyLoading() : `${interestedCount} / 140`}
       </p>
-      <a
-        href="https://tally.so/r/w5GKoo"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={`
+      {interestedCount < 140 && (
+        <a
+          href="https://tally.so/r/w5GKoo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`
             ${turretRoad.className} text-[24px] font-bold
             bg-[#E92EFB] text-white
             px-6 py-2 rounded-full
             hover:bg-opacity-80 transition-all duration-300
             date-glow text-shadow inline-block
           `}
-      >
-        Sign up
-      </a>
+        >
+          Sign up
+        </a>
+      )}
     </div>
   );
 };
