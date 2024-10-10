3228e3be1708970ba6175ca5c90464a157b13840
Milan <milan.barande@gmail.com>
2024-10-10 09:33:44 +0200

    initialise count to null

--- src/app/components/InterestedCard.tsx
+++ src/app/components/InterestedCard.tsx
@@ -12,7 +12,7 @@ const turretRoad = Turret_Road({
 });
 
 const InterestedCard: React.FC = () => {
-  const [interestedCount, setInterestedCount] = useState(0);
+  const [interestedCount, setInterestedCount] = useState<number | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetchInterestedCount();
@@ -31,11 +31,15 @@ const InterestedCard: React.FC = () => {
 
   const renderWavyLoading = () => {
     return (
-      <span className={styles.wavyText}>
-        {'Loading...'.split('').map((char, index) => (
-          <span key={index}>{char}</span>
-        ))}
-      </span>
+      <div
+        className={`${turretRoad.className} text-[32px] text-black mb-6 date-glow min-h-[100px]`}
+      >
+        <span className={styles.wavyText}>
+          {'Loading...'.split('').map((char, index) => (
+            <span key={index}>{char}</span>
+          ))}
+        </span>
+      </div>
     );
   };
 
@@ -51,26 +55,32 @@ const InterestedCard: React.FC = () => {
       >
         Find 140 Explorers to take part in the games!
       </p>
-      <p
-        className={`${turretRoad.className} text-[32px] text-black mb-6 date-glow`}
-      >
-        {isLoading ? renderWavyLoading() : `${interestedCount} / 140`}
-      </p>
-      {interestedCount < 140 && (
-        <a
-          href="https://tally.so/r/w5GKoo"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`
+      {interestedCount === null ? (
+        renderWavyLoading()
+      ) : (
+        <>
+          <p
+            className={`${turretRoad.className} text-[32px] text-black mb-6 date-glow`}
+          >
+            {`${interestedCount} / 140`}
+          </p>
+          {interestedCount < 140 && (
+            <a
+              href="https://tally.so/r/w5GKoo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`
             ${turretRoad.className} text-[24px] font-bold
             bg-[#E92EFB] text-white
             px-6 py-2 rounded-full
             hover:bg-opacity-80 transition-all duration-300
             date-glow text-shadow inline-block
           `}
-        >
-          Sign up
-        </a>
+            >
+              Sign up
+            </a>
+          )}
+        </>
       )}
     </div>
   );
